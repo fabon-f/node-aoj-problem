9cf30083fbc2e0275d31887e9658042598eca91f
hinamiyagk <syobon.hinata.public@gmail.com>
2016-09-25 08:51:17 +0900

    Fix issue that problem 0104 can't be parsed correctly

--- src/parse-problem-page.js
+++ src/parse-problem-page.js
@@ -2,6 +2,11 @@ const parseHTML = require("libxmljs").parseHtml;
 const Iconv = require("iconv").Iconv;
 const sjis = new Iconv("SJIS", "UTF-8");
 module.exports = function parseProblemPage(body) {
-    const html = sjis.convert(body).toString().replace("</nobr>", "").replace("SHIFT-JIS", "UTF-8").replace(/<script type="text\/javascript">[\s\S]*?<\/script>/g, "").replace("id=\"body\"", "id=\"body2\"");
+    const html = sjis.convert(body).toString()
+        .replace("</nobr>", "")
+        .replace("SHIFT-JIS", "UTF-8")
+        .replace(/<script type="text\/javascript">[\s\S]*?<\/script>/g, "")
+        .replace("id=\"body\"", "id=\"body2\"")
+        .replace(/<pre>([\s\S]+?)<\/pre>/g, (match, submatch) => `<pre>${submatch.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;")}</pre>`);
     return parseHTML(html);
 };
