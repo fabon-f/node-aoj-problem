5b2a9a912a6fc08d0d7acf84723320f8ba671376
hinamiyagk <syobon.hinata.public@gmail.com>
2016-09-25 18:32:27 +0900

    Replace native module dependencies with pure-js packages

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
-const url = require("url");
+const { resolve: resolveURL } = require("url");
 const request = require("request");
-const parseProblemPage = require("./parse-problem-page");
+const iconv = require("iconv-lite");
+const htmlparser = require("htmlparser2");
+const {
+    getText,
+    getInnerHTML,
+    getElementsByTagName,
+    getElements: getElementsByAttribute,
+    getElementsByTagType,
+    removeElement
+} = require("domutils");
+
 const langToParam = lang => {
     if (lang === "ja") { return "jp"; }
     if (lang === "en") { return "en"; }
     throw new Error("Invalid language");
 };
+
 module.exports = function getAOJProblemInfo(problemID, language) {
     const problemURL = `http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=${problemID}&lang=${langToParam(language)}`;
     return new Promise((resolve, reject) => {
@@ -13,28 +24,32 @@ module.exports = function getAOJProblemInfo(problemID, language) {
             url: problemURL,
             encoding: null
         }, (error, response, body) => {
-            if (error) { reject(error); }
-            const problemPage = parseProblemPage(body);
-            const title = problemPage.find("//title")[0].text().replace(/ \| Aizu Online Judge$/, "");
-            for (const link of problemPage.find("//div[@class = \"description\"]//a")) {
-                const hrefAttribute = link.attr("href");
-                hrefAttribute.value(url.resolve(problemURL, hrefAttribute.value()));
+            if (error) {
+                reject(error);
+                return;
             }
-            for (const img of problemPage.find("//div[@class = \"description\"]//img")) {
-                const srcAttribute = img.attr("src");
-                srcAttribute.value(url.resolve(problemURL, srcAttribute.value()));
-            }
-            for (const comment of problemPage.find("//div[@class = \"description\"]//comment()")) {
-                comment.remove();
-            }
-            const description = problemPage.get("//div[@class = \"description\"]");
-            if (!description) {
+            const problemPage = htmlparser.parseDOM(iconv.decode(body, "shift_jis"));
+            const title = getText(getElementsByTagName("title", problemPage)[0])
+                .replace(/ \| Aizu Online Judge$/, "");
+            const descriptionNode = getElementsByAttribute({
+                class: "description"
+            }, problemPage)[0];
+            if (!descriptionNode) {
                 reject(new Error("Problem doesn't exist"));
                 return;
             }
+            for (const link of getElementsByTagName("a", descriptionNode)) {
+                link.attribs.href = resolveURL(problemURL, link.attribs.href);
+            }
+            for (const img of getElementsByTagName("img", descriptionNode)) {
+                img.attribs.src = resolveURL(problemURL, img.attribs.src);
+            }
+            for (const comment of getElementsByTagType("comment", descriptionNode)) {
+                removeElement(comment);
+            }
             resolve({
                 title,
-                description: description.childNodes().map(childNode => childNode.toString()).join("").trim()
+                description: getInnerHTML(descriptionNode).trim()
             });
         });
     });
