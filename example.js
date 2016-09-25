4072a874603ca920f9263b565c44d48360c7ee51
hinamiyagk <syobon.hinata.public@gmail.com>
2016-09-25 22:00:03 +0900

    Update example.js

--- example.js
+++ example.js
@@ -1,7 +1,15 @@
-/* eslint-disable no-unused-expressions */
+/* eslint-disable no-console */
 
 const getAOJProblemInfo = require(".");
-getAOJProblemInfo("0104", "ja").then(data => {
-    data.title; // title of problem
-    data.description; // description of problem(HTML)
+
+// http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=0104
+getAOJProblemInfo("0104", "en").then(data => {
+    console.log(data.title);
+    // Magical Tiles
+
+    console.log(data.description);
+    // <h1>Magical Tiles</h1>
+    // <p>
+    // There is a magic room in a homestead. The room is paved with H &times W tiles. There are five different tiles:
+    // </p> etc...
 });
